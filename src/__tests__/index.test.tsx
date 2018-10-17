56ed4ab80bae23c5bd452ab6f883af309f02f5d3
rakannimer <rakannimer@gmail.com>
2018-10-17 18:33:09 +0300

    chore: Add unmount test.

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, cleanup } from "react-testing-library";
 import ReactKeyMaster from "../";
 
 describe("React-Keymaster", () => {
@@ -15,6 +15,14 @@ describe("React-Keymaster", () => {
       which: 65
     });
     expect(onKeyDown.mock.calls.length).toEqual(1);
+    fireEvent.keyUp(container, {
+      key: "a",
+      keyCode: 65,
+      which: 65
+    });
+    expect(onKeyUp.mock.calls.length).toEqual(1);
+    await cleanup();
+    // After unmount stops listening
     fireEvent.keyUp(container, {
       key: "a",
       keyCode: 65,
